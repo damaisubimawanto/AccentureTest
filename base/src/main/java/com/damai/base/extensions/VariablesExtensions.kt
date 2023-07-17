7561384c51cd576ff6d1703be893ea1e716f7c7d
Damai Subimawanto <damai.subimawanto@gmail.com>
2023-07-18 02:11:27 +0700

    Imported the direct variables of NEXT_PATTERN and QUERY_PARAM_SINCE in VariablesExtensions.kt.

--- base/src/main/java/com/damai/base/extensions/VariablesExtensions.kt
+++ base/src/main/java/com/damai/base/extensions/VariablesExtensions.kt
@@ -1,7 +1,8 @@
 package com.damai.base.extensions
 
 import android.net.Uri
-import com.damai.base.utils.Constants
+import com.damai.base.utils.Constants.NEXT_PATTERN
+import com.damai.base.utils.Constants.QUERY_PARAM_SINCE
 
 /**
  * Created by damai007 on 11/July/2023
@@ -14,11 +15,11 @@ fun Int?.orZero() = this ?: 0
 //region String values
 fun String?.nextLinkKey(): Int? {
     return if (this?.contains("rel=\"next\"") == true) {
-        val matchResult = Constants.NEXT_PATTERN.find(this)
+        val matchResult = NEXT_PATTERN.find(this)
         val resultValue = matchResult?.value
         resultValue?.let { _resultValue ->
             val nextLinkUri = Uri.parse(_resultValue)
-            nextLinkUri.getQueryParameter(Constants.QUERY_PARAM_SINCE)?.toIntOrNull()
+            nextLinkUri.getQueryParameter(QUERY_PARAM_SINCE)?.toIntOrNull()
         }
     } else null
 }
